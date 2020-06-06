b569fdbf77071a5976e7096c533f770baa98c201
Sultan Ali <sultanjamali1986@gmail.com>
2020-06-07 00:33:50 +0300

    adding progress toolbar

--- project4/ViewController.swift
+++ project4/ViewController.swift
@@ -25,11 +25,38 @@ class ViewController: UIViewController , WKNavigationDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "open", style: .plain, target: self, action: #selector(openTapped))
+        
         let url = URL(string: "https://www.google.com/")
         webView.load(URLRequest(url: url!))
         webView.allowsBackForwardNavigationGestures = true
     }
+    
+    @objc func openTapped() {
+        
+        let ac = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        let openYoutube = UIAlertAction(title: "Youtube.com", style: .default, handler: openPage)
+        let openDailymotion = UIAlertAction(title: "Dailymotion.com", style: .default, handler: openPage)
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel)
+        
+        ac.addAction(openYoutube)
+        ac.addAction(openDailymotion)
+        ac.addAction(cancel)
+        
+        present(ac , animated: true)
+        
+    }
 
+    func openPage(action: UIAlertAction){
+        
+        let url = URL(string: "https://www." + action.title!)!
+        webView.load(URLRequest(url: url))
+    
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
+    }
 
 }
 
